5cde0cb5e19ff6504209786361b1c6083d36829a
daiqin <daiqin@58.com>
2022-02-21 17:23:08 +0800

    feat yun

--- routes/yun/controller.js
+++ routes/yun/controller.js
@@ -2,7 +2,7 @@ const mysql = require('../../utils/mysql')
 const table = 'yun'
 
 exports.get = async ctx => {
-  const data = await mysql(`SELECT * FROM ${table} ORDER BY id`)
+  const data = await mysql(`SELECT * FROM ${table} ORDER BY id DESC`)
   const [{ count }] = await mysql(`SELECT COUNT(*) as count FROM ${table}`)
   ctx.body = { todos: data, total: count }
 }
