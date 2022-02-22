113fe7d000cf804edcbc882a8eb132df076a7b01
daiqin <daiqin@58.com>
2022-02-22 11:16:56 +0800

    feat publish

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,14 @@ router.get("/", async (ctx, next) => {
   });
 });
 
+router.post('/api/publish', async (ctx) => {
+  const { file } = ctx.request.files
+  	// 正则 替换掉文件原始路径中不需要的部分
+	const reg = new RegExp(".*/public/", "g");
+  
+  ctx.body = file?.path?.replace(reg, "") ?? ''
+})
+
 router.get("/string", async (ctx, next) => {
   ctx.body = "koa2 string";
 });
