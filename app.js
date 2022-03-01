aaa00e2f7c6524675f54b45e3a2cb88c70088047
daiqin <daiqin@58.com>
2022-03-01 15:32:54 +0800

    feat mclub

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const Koa = require('koa')
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const koaBody = require('koa-body')
 const logger = require('koa-logger')
-const auth = require('./utils/auth')
 const cors = require('koa2-cors')
 const path = require('path')
 const jwt = require('jsonwebtoken')
+
 // 路由
 const index = require('./routes/index')
 const user = require('./routes/user')
 const resources = require('./routes/resources')
 const rank = require('./routes/rank')
-const yellow = require('./routes/yellow')
 const admin = require('./routes/admin')
-const yun = require('./routes/yun')
 const comment = require('./routes/comment')
+const mClub = require('./routes/mClub')
 
 // error handler
 onerror(app)
@@ -39,8 +37,6 @@ app.use(logger())
 app.use(
 	require('koa-mount')('/api', require('koa-static')(__dirname + '/public'))
 )
-// app.use(auth())
-app.use(views(__dirname + '/views', { extension: 'pug' }))
 
 // logger
 app.use(async (ctx, next) => {
@@ -53,10 +49,6 @@ app.use(async (ctx, next) => {
 		ctx.auth = userInfo
 	}
 	await next()
-	// const start = new Date()
-	// await next()
-	// const ms = new Date() - start
-	// console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
 // routes
@@ -64,10 +56,9 @@ app.use(index.routes(), index.allowedMethods())
 app.use(user.routes(), user.allowedMethods())
 app.use(resources.routes(), resources.allowedMethods())
 app.use(rank.routes(), rank.allowedMethods())
-app.use(yellow.routes(), yellow.allowedMethods())
 app.use(admin.routes(), admin.allowedMethods())
-app.use(yun.routes(), yun.allowedMethods())
 app.use(comment.routes(), comment.allowedMethods())
+app.use(mClub.routes(), mClub.allowedMethods())
 
 app.listen(3000)
 // module.exports = app
